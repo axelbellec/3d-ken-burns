5a6f4320ae752044e3b9ed9949ce1e2c836a2f7c
Axel Bellec <axel.bellec@cdiscount.com>
2020-01-03 14:48:40 +0100

    keep zoom in but remove zoom out

--- autozoom.py
+++ autozoom.py
@@ -93,6 +93,10 @@ if __name__ == '__main__':
 		'objectTo': objectTo,
 		'boolInpaint': True
 	})
+	sequence = [ 
+		numpyFrame[:, :, ::-1] 
+		for numpyFrame in numpyResult #+ list(reversed(numpyResult))[1:] 
+	]
 
-	moviepy.editor.ImageSequenceClip(sequence=[ numpyFrame[:, :, ::-1] for numpyFrame in numpyResult + list(reversed(numpyResult))[1:] ], fps=25).write_videofile(arguments_strOut)
+	moviepy.editor.ImageSequenceClip(sequence=sequence, fps=25).write_videofile(arguments_strOut)
 # end
